73a9eedf257045dce7aaa19735ffe50751fdc86d
kallso <costeskevin31@gmail.com>
2021-03-16 23:41:01 +0100

    Exo Memoire presque fini reste commentaires a faire

--- exoMemoire.c
+++ exoMemoire.c
@@ -12,10 +12,13 @@ int main( void ) {
     printf("CONSTANTE1 : %i \n", CONSTANTE1);
     // CONSTANTE1 est definie grace a une macro (#define) qui remplacera toutes les occurences de CONSTANTE1 dans
     // le code par la valeur associée lors de la phase de prepocesseur.
-    printf("CONSTANTE2 : %i \n", CONSTANTE2);
-    // CONSTANTE2 est une constante global.
-    printf("CONSTANTE3 : %i \n", CONSTANTE3);
-    // CONSTANTE3 est une constante local.
+
+    //printf("CONSTANTE1 : %p \n", &CONSTANTE1); Impossible de recuperer l'adresse d'un int (100 dans ce cas)
+    // &100 est impossible.
+    printf("CONSTANTE2 : %p \n", &CONSTANTE2);
+    // CONSTANTE2 est une constante global. On peut recuperer son adresse memoire.
+    printf("CONSTANTE3 : %p \n", &CONSTANTE3);
+    // CONSTANTE3 est une constante local. On peut recuperer son adresse memoire.
 
     // CONSTANTE1 = 0; // impossible : CONSTANTE1 n'est pas une variable, c'est un marqueur pour la macro #define, on
     // ne peut rien lui affecter.
@@ -30,7 +33,7 @@ int main( void ) {
     /* CONSTANTE1 sera remplacer par 100 par le preprocesseur.
     Il est donc incorect d'essayer de recuperer l'adresse d'un int (ce n'est pas une variable). */
 
-    //pCONSTANTE2 = &CONSTANTE2;
+    // pCONSTANTE2 = &CONSTANTE2;
     /* Cette assignation genere un warning car le compilateur detecte deja qu'un pointeur
      * qui n'est pas const pointe sur une constante, ce qui ouvre la possiblité d'acceder a la constante en ecriture.
      */
@@ -40,10 +43,10 @@ int main( void ) {
      * Le systeme d'exploitation remarque alors notre maneouvre illégale sur la memoire et nous tue en nous envoyant un
      * Signal: SIGSEGV (Segmentation fault). (punition pour avoir essayé une manoeuvre illegal en memoire). */
 
-    pCONSTANTE3 = &CONSTANTE3;
+    // pCONSTANTE3 = &CONSTANTE3;
     /* Cette assignation genere aussi un warning car le compilateur detecte deja qu'un pointeur
      * qui n'est pas const pointe sur une constante, ce qui ouvre la possiblité d'acceder a la constante en ecriture.*/
-    *pCONSTANTE3 = 2021;
+    // *pCONSTANTE3 = 2021;
     /* On essaie d'ecrire dans l'emplacement memoire pointé par la constante et notre pointeur.
      * et ON Y ARRIVE ! Car, étant une variable locale, elle est déclarée sur la stack et est donc modifiable.
      * Le systeme d'exploitation ne remarque rien du tout, tout est normal selon lui. */
@@ -64,4 +67,22 @@ int main( void ) {
      *  On remarque aussi que tab1[n] est equivalent a  *tab + n car on voit bien que cette syntaxe n'opere en fait
      *  qu'a un decalage depuis l'adresse du premier element du tableau (pointeur tab1).
      */
+
+    printf("\n");
+
+    for (int i = -10; i < 11; ++i) {
+        printf("tab2[%i] = %p : %i \n", i, &tab2[i], tab1[i]);
+    }
+
+    /* TODO expliquer
+     *
+     */
+
+    int *pV1 = &v1;
+    pV1 = &CONSTANTE3;
+    *pV1 = 2022;
+
+    printf("pv1 = %p, sa valeur pointé : %i \n", pV1, *pV1);
+    printf("CONSTANTE3 = %i \n", CONSTANTE3);
+
 }
